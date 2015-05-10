419ca7bea18b6953de2b0247913f32a963df40ec
Scott Waite <scottwaite@fullsail.edu>
2015-05-10 18:02:07 -0400

    Music plays in notification service

--- Week 1/Project/AudioService/app/src/main/java/com/scottwaite/audioservice/MainActivity.java
+++ Week 1/Project/AudioService/app/src/main/java/com/scottwaite/audioservice/MainActivity.java
@@ -1,5 +1,10 @@
 package com.scottwaite.audioservice;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -11,6 +16,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -95,14 +101,52 @@ public class MainActivity extends ActionBarActivity {
             return rootView;
         }
 
+
+
+
+
+
+
+
+
         public void HandlePreviousButtonClick(){
             Log.i(TAG, "previous button clicked");
         }
 
+
+
+
+
+
+
+
+
+
+
         public void HandlePlayButtonClick(){
+            //show string in logcat
             Log.i(TAG, "play button clicked");
+            //play the song
             playAudio();
+            //display the toast for Play
+            initNotification();
+            Toast.makeText(getApplicationContext(), "Play", Toast.LENGTH_SHORT).show();
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
         public void HandleStopButtonClick(){
             Log.i(TAG, "stop button clicked");
@@ -114,14 +158,61 @@ public class MainActivity extends ActionBarActivity {
 
     }
 
+
+
+
+
+
+
+
+
+
+
+
     private void playAudio() {
         MediaPlayer player = MediaPlayer.create(this, R.raw.thirteenthirtyfive);
         player.start();
         player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
             public void onCompletion(MediaPlayer mp) {
+                mp.pause();
                 mp.release();
             }
         });
+    }
+
+
+
+
+    // Setup the ID for the notification bar
+    private static final int NOTIFICATION_ID = 1;
+
+
+
+    // Create a notification so the app can be accessed from the notification bar
+    private void initNotification() {
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+        int icon = R.drawable.ic_launcher;
+        CharSequence tickerText = "Audio Service";
+        long when = System.currentTimeMillis();
+        Notification notification = new Notification(icon, tickerText, when);
+        notification.flags = Notification.FLAG_ONGOING_EVENT;
+        Context context = getApplicationContext();
+        CharSequence contentTitle = "Audio Service";
+        CharSequence contentText = "Return to Player";
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context, 0,
+                notificationIntent, 0);
+        notification.setLatestEventInfo(context, contentTitle, contentText,
+                contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    // Clear notification
+    private void cancelNotification() {
+        String ns = Context.NOTIFICATION_SERVICE;
+        NotificationManager mNotificationManager = (NotificationManager) getSystemService(ns);
+        mNotificationManager.cancel(NOTIFICATION_ID);
     }
 }
